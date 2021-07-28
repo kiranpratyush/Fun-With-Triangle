7c2a40b96a1d92ebf0e6956c8f713e3aac1959b9
kiranpratyush <pratyushkiranrout@gmail.com>
2021-07-28 20:14:48 +0530

    feat:area of Triangle

---
+++ calculateArea/script.js
@@ -0,0 +1,31 @@
+const inputLegth1 =document.querySelector("#length1");
+const inputLegth2 =document.querySelector("#length2");
+const inputLegth3 =document.querySelector("#length3");
+const btnSubmit = document.querySelector(".submit");
+const result = document.querySelector(".result");
+
+
+function calArea(length1,length2,length3)
+{
+    let s = (length1+length2+length3)/2;
+    let areaSquare = s*(s-length1)*(s-length2)*(s-length3);
+    return Math.round(Math.sqrt(areaSquare));
+}
+
+
+btnSubmit.addEventListener("click",function(){
+    length1 = inputLegth1.value;
+    length2 = inputLegth2.value;
+    length3 = inputLegth3.value;
+    let area = calArea(Number(length1),Number(length2),Number(length3));
+    if(area)
+    {
+        result.textContent = "Area is: "+ area;
+    }
+    else{
+        result.textContent ="Provided Lengths can't form a Triangle";
+    }
+
+    
+
+});
